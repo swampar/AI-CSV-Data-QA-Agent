3183b6508bed2576f4cd2e8c5ecfa5b35e662cc0
sudeepa <sudeepm831@gmail.com>
2026-08-04 11:05:15 +0530

    Implemented CSV upload API

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
+from data_loader import load_csv
 
-app = FastAPI(
-    title="AI CSV Data Q&A Agent",
-    version="1.0"
-)
+app = FastAPI(title="AI CSV Data Q&A Agent")
+
+UPLOAD_FOLDER = "uploads"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 
 @app.get("/")
 def home():
+    return {"message": "AI CSV Data Q&A Agent is Running!"}
+
+
+@app.post("/upload")
+async def upload_csv(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    df = load_csv(file_path)
+
     return {
-        "message": "AI CSV Data Q&A Agent is Running!"
+        "filename": file.filename,
+        "rows": len(df),
+        "columns": list(df.columns)
     }
